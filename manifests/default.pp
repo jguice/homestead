4835d6289649644c8487bcd26f65d0982eb85d40
jguice <jguice@gmail.com>
2015-11-29 07:56:34 -0800

    added db script and bash profile

--- manifests/default.pp
+++ manifests/default.pp
@@ -1,3 +1,21 @@
-package {'silversearcher-ag':
-   ensure => 'installed'
+package { 'silversearcher-ag':
+  ensure => 'installed'
+}
+
+file { 'db script':
+  path    => '/usr/local/bin/d',
+  mode    => '0755',
+  content => "#!/bin/bash
+    mysql -uhomestead -psecret
+  "
+}
+
+file { 'bash_profile':
+  path    => '/home/vagrant/.bash_profile',
+  mode    => '0644',
+  owner   => 'vagrant',
+  group   => 'vagrant',
+  content => 'set -o vi
+    export EDITOR=vim
+    . ~/.bashrc'
 }
